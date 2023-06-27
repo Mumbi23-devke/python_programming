49c4cee21c1b0849c1c07702f07862259191d417
eMobilis <gloriawaweru5@gmail.com>
2023-06-27 09:44:53 +0300

    intro to python

---
+++ betting.py
@@ -0,0 +1,17 @@
+#create a simple reward system
+#when the user enters any number between 1 and 5
+#they get a reward
+
+number = int(input("Enter a number  "))
+
+if number == 1:
+    print("You win a car!!! ")
+elif number == 2:
+    print("You win sh.10,000 !! ")
+elif number == 3:
+    print("You get a voucher worth sh.5,000!! ")
+elif number == 4:
+    print("You win a HP laptop!")
+elif number == 5:
+    print("You win a samsung galaxy!")
+
