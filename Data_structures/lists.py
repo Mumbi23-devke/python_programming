7a883ca389bd6f2de1bf2250073dfe6dd4b9a72a
eMobilis <gloriawaweru5@gmail.com>
2023-06-29 09:53:10 +0300

    lists

---
+++ Data_structures/lists.py
@@ -0,0 +1,11 @@
+#defined using squared brackets
+
+friends = ["Janet", "Mitchele", "Natasha", " Mike"]
+
+#you can modify data in a list (add,remove or update)
+
+friends.append('Kelly')
+friends.append("Joe")
+friends.remove("Janet")
+
+print(friends)
