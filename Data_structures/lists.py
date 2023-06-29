8ac975db689d9e08d0bee67c095a86ea4ab4381f
eMobilis <gloriawaweru5@gmail.com>
2023-06-29 10:25:23 +0300

    lists

--- Data_structures/lists.py
+++ Data_structures/lists.py
@@ -1,11 +1,19 @@
 #defined using squared brackets
 
-friends = ["Janet", "Mitchele", "Natasha", " Mike"]
+friends = ["Janet", "Mitchele", "Natasha", " Mike", "Faith" , "Susie"]
 
 #you can modify data in a list (add,remove or update)
 
 friends.append('Kelly')
-friends.append("Joe")
-friends.remove("Janet")
+friends.append("Bob")
+friends.append("Gill")
 
+print(friends)
+
+#counting starts from index zero
+
+print(friends[5])
+print(friends[6])
+print(friends[-5])
+friends.remove("Janet")
 print(friends)
---
+++ Data_structures/tuples.py
@@ -0,0 +1,16 @@
+#defined by normal brackets ()
+
+myteam = ("Mumbi", "Reggie", "Stella", "Keith")
+
+#they cannot be modified
+#keeps the order
+
+print(myteam)
+
+
+
+
+
+
+
+
