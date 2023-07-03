ecc0db449326e460f10b86dc9cb779e45b37f49d
eMobilis <gloriawaweru5@gmail.com>
2023-07-03 10:32:00 +0300

    functions without parameters

---
+++ Functions/functions with parameters.py
@@ -0,0 +1,13 @@
+def addnums(x, y):
+    result = x + y
+    print(result)
+
+
+addnums(int(input("Enter a number ")), int(input("Enter another number ")))
+
+
+def name(m, w):
+    print(m, w)
+
+
+name(input('Enter first name '), input('Enter last name '))
