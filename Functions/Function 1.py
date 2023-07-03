ace03329355e3656bff6da60860e060142efcf00
eMobilis <gloriawaweru5@gmail.com>
2023-07-03 09:43:59 +0300

    function

---
+++ Functions/Function 1.py
@@ -0,0 +1,31 @@
+def sayhello():
+    print("Welcome back!!")
+
+
+sayhello()
+
+
+def morning():
+    print("Wake up sleepyhead!!")
+
+
+morning()
+
+
+def calc_months():
+    age = float(input("Enter your age "))
+    months = age * 12
+    print(f"You have lived {months} months")
+
+
+calc_months()
+
+
+
+
+
+
+
+
+
+
