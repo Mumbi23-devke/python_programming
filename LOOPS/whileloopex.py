1fea720c75be8e4eb4a8c06d232e10daa0452d39
eMobilis <gloriawaweru5@gmail.com>
2023-07-03 09:11:28 +0300

    loop

---
+++ LOOPS/whileloopex.py
@@ -0,0 +1,16 @@
+#using range 1 - 1000
+#print all numbers using while loop
+#ensure user inputs number in terminal
+
+x = int(input("Enter first number "))
+y = int(input("Enter second number "))
+
+while x <= y:
+    print(x)
+    x += 1
+
+
+
+
+
+
